e328043dd6e1a535af453994ac14a125d374deda
harshitanchan <harshitanchan647@gmail.com>
2024-07-10 23:30:04 +0530

    react tut

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./assets/Food";
+function App() {
+
+return(
+  <>
+  <Header />
+  <Food />
+
+  <Footer />
+
+  </>
+
+);
+
+}
+
+export default App
